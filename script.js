edfcad329a3699b3dd80b4b398a37bd78d8134b0
nikola <nicksona.in.lan@gmail.com>
2019-01-17 12:15:40 +0200

    This are first files of the project, but are not related with it. The common is that the project must have web UI.I am just training my git skills.

---
+++ script.js
@@ -0,0 +1,46 @@
+//root scope = window scope
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var btnRandom = document.querySelector("#btnRandom");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function setRandomColours(event) {
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+    setGradient();
+}
+
+body.style.background = "linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+css.textContent = body.style.background + ";";
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+btnRandom.addEventListener("click",setRandomColours);
